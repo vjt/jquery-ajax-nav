5b36f0d69f327f1324ba4be6cf8bd0499770b1a0
Marcello Barnaba <vjt@openssl.it>
2011-03-14 18:16:23 +0100

    Fillscreen: refactor and make it an collection method

--- jquery.fill-screen.js
+++ jquery.fill-screen.js
@@ -2,26 +2,48 @@
 //
 //  - vjt  Mon Mar 14 18:04:04 CET 2011
 
-/**
- * Dynamically adjusts the height of the #content container
- * to the available maximum,  after subtracting the #header
- * and the #footer heights.
- *
- * You can pass the IDs via options.header, options.content
- * and options.footer and and you can also pass the content
- * dimensions via options.inner and options.outer.
- *
- * If the calculated dimensions are *less* than the actual
- * ones, nothing is performed.
- */
 (function ($) {
 
-  $.fillScreen = function () {
+  /**
+   * Repeatedly calls the fill () method when the window is
+   * resized. Evaluates the content height *once*.
+   */
+  $.fn.fillScreen = function () {
+    var options = arguments[0] || {};
+    var content = $(this);
+    var options = $.extend ({
+      inner : content.height (),
+      outer : content.outerHeight (),
+
+      header: '#header',
+      footer: '#footer'
+    }, options);
+
+    fill (options);
+
+    $(window).resize (function () {
+      fill (options);
+    });
+  };
+
+  /**
+   * Dynamically adjusts the height of the #content container
+   * to the available maximum,  after subtracting the #header
+   * and the #footer heights.
+   *
+   * You can pass the IDs via options.header, options.content
+   * and options.footer and and you can also pass the content
+   * dimensions via options.inner and options.outer.
+   *
+   * If the calculated dimensions are *less* than the actual
+   * ones, nothing is performed.
+   */
+  var fill = function () {
     var options = arguments[0] || {};
 
-    var header  = $(options.header  || '#header' );
-    var content = $(options.content || '#content');
-    var footer  = $(options.footer  || '#footer' );
+    var header  = $(options.header );
+    var content = $(options.content);
+    var footer  = $(options.footer );
 
     var inner   = options.inner || $(content).height ();
     var outer   = options.outer || $(content).outerHeight ();
@@ -31,33 +53,6 @@
 
     if (inner < changed)
       $(content).height (changed);
-  };
-
-  /**
-   * Repeatedly calls the $.fillScreen () method when the window
-   * is resized. Evaluates *once* the content height, and passes
-   * it to the $.fillScreen () method on every invocation.
-   */
-  $.keepScreenFilled = function () {
-    var options = arguments[0] || {};
-    var content = $(options.content || '#content');
-    var options = $.extend ({
-      inner: content.height (),
-      outer: content.outerHeight ()
-    }, options);
-
-    $.fillScreen (options);
-    $(window).resize (function () {
-      $.fillScreen (options);
-    });
-  };
-
-  /**
-   * Reset the fill on the given content, or #content
-   */
-  $.resetFill = function () {
-    var options = arguments[0] || {};
-    $(options.content || '#content').attr ('style', '');
   };
 
 }) (jQuery);
